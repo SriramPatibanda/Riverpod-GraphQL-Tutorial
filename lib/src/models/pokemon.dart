43a57131bfcf15f4c6460965c1d2bd526e9ded84
Sriram Patibanda <patibandasriram2k02@gmail.com>
2021-11-19 22:55:47 +0530

    Add Pokemon Model

---
+++ lib/src/models/pokemon.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'pokemon.freezed.dart';
+part 'pokemon.g.dart';
+
+@freezed
+class Pokemon with _$Pokemon {
+  const factory Pokemon({
+    required String name,
+    required String sprite,
+    required String backSprite,
+    required Map<String, String> abilities,
+  }) = _Pokemon;
+
+  factory Pokemon.fromJson(Map<String, dynamic> json) =>
+      _$PokemonFromJson(json);
+}
